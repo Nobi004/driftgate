353d55066809e8c497357ca4d9cf157dc60bbfe5
Nobi004 <mahmudun.nobi@brainify.tech>
2026-06-21 01:47:30 +0600

    Update cmd.go

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,6 +1,6 @@
 package cmd
 
 // Execute is the entry point for the driftgate application
-func Execute() {
-	// TODO: Implement command execution
-}
+// func Execute() {
+// 	// TODO: Implement command execution
+// }
